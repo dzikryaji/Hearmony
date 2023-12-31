f90ee1f453376e51545f9be3dd7ad58aceb73707
dzikryaji <dzikryaji07@gmail.com>
2023-12-31 15:02:26 +0700

    Create splash screen

---
+++ app/src/main/java/das/mobile/hearmony/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package das.mobile.hearmony;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // make status bar transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        Handler h = new Handler(Looper.getMainLooper());
+        h.postDelayed(()->{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }, 5000L);
+
+    }
+}
