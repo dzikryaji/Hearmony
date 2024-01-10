0d915a254b7201e2327f56db12190b58ed2b60f7
dzikryaji <dzikryaji07@gmail.com>
2024-01-10 20:20:17 +0700

    add detail article layout

---
+++ app/src/main/java/das/mobile/hearmony/activity/DetailArticleActivity.java
@@ -0,0 +1,16 @@
+package das.mobile.hearmony.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import das.mobile.hearmony.R;
+
+public class DetailArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_article);
+    }
+}
--- app/src/main/java/das/mobile/hearmony/adapter/InsightAdapter.java
+++ app/src/main/java/das/mobile/hearmony/adapter/InsightAdapter.java
@@ -1,23 +1,31 @@
 package das.mobile.hearmony.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import das.mobile.hearmony.activity.DetailArticleActivity;
 import das.mobile.hearmony.databinding.ItemInsightBinding;
 
 public class InsightAdapter extends RecyclerView.Adapter<InsightAdapter.InsightViewHolder> {
+    Context context;
     @NonNull
     @Override
     public InsightViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         return new InsightViewHolder(ItemInsightBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull InsightViewHolder holder, int position) {
-        // TODO: Bind Data into View Holder
+        holder.binding.getRoot().setOnClickListener(view ->{
+            Intent intent = new Intent(context, DetailArticleActivity.class);
+            context.startActivity(intent);
+        });
     }
 
     @Override
