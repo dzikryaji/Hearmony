29e2eaf31ef95c8de55e0234265d7fc37f784ed6
dzikryaji <dzikryaji07@gmail.com>
2024-01-16 12:57:30 +0700

    get psychologist list

--- app/src/main/java/das/mobile/hearmony/adapter/PsychologistAdapter.java
+++ app/src/main/java/das/mobile/hearmony/adapter/PsychologistAdapter.java
@@ -8,21 +8,31 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 import das.mobile.hearmony.activity.DoctorProfileActivity;
 import das.mobile.hearmony.databinding.ItemPsychologistBinding;
+import das.mobile.hearmony.model.Psikolog;
 
 public class PsychologistAdapter extends RecyclerView.Adapter<PsychologistAdapter.PsychologistViewHolder> {
-    private Context context;
+    private final Context context;
+    private final List<Psikolog> psikologList;
+
+    public PsychologistAdapter(Context context, List<Psikolog> psikologList) {
+        this.context = context;
+        this.psikologList = psikologList;
+    }
 
     @NonNull
     @Override
     public PsychologistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        this.context = parent.getContext();
         return new PsychologistViewHolder(ItemPsychologistBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull PsychologistViewHolder holder, int position) {
+        Psikolog psikolog = psikologList.get(position);
+        holder.binding.tvName.setText(psikolog.getName());
         holder.binding.btnMakeAppointment.setOnClickListener(view -> {
             Intent intent = new Intent(context, DoctorProfileActivity.class);
             context.startActivity(intent);
@@ -31,7 +41,7 @@ public class PsychologistAdapter extends RecyclerView.Adapter<PsychologistAdapte
 
     @Override
     public int getItemCount() {
-        return 5;
+        return psikologList.size();
     }
 
     public class PsychologistViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/das/mobile/hearmony/fragment/ChatConsultationFragment.java
+++ app/src/main/java/das/mobile/hearmony/fragment/ChatConsultationFragment.java
@@ -8,12 +8,25 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 import das.mobile.hearmony.adapter.PsychologistAdapter;
 import das.mobile.hearmony.databinding.FragmentChatConsultationBinding;
+import das.mobile.hearmony.model.Psikolog;
 
 public class ChatConsultationFragment extends Fragment {
 
     FragmentChatConsultationBinding binding;
+    PsychologistAdapter adapter;
+    ArrayList<Psikolog> psychologistList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,9 +37,47 @@ public class ChatConsultationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentChatConsultationBinding.inflate(inflater, container, false);
-        binding.rvPsychologist.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.rvPsychologist.setAdapter(new PsychologistAdapter());
+        psychologistList = new ArrayList<>();
+        setUpFirebaseRecyclerView();
         // Inflate the layout for this fragment
         return binding.getRoot();
+    }
+
+    private void setUpFirebaseRecyclerView() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("psikolog");
+        Query query = databaseReference.orderByChild("name");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                updatePsychologistList(dataSnapshot);
+                setUpAdapter();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle error if needed
+            }
+        });
+    }
+
+    private void updatePsychologistList(DataSnapshot dataSnapshot) {
+        psychologistList.clear();
+
+        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            Psikolog psikolog = snapshot.getValue(Psikolog.class);
+            if (psikolog != null) {
+                psychologistList.add(psikolog);
+            }
+        }
+
+        Collections.reverse(psychologistList);
+    }
+
+
+    private void setUpAdapter() {
+        adapter = new PsychologistAdapter(getActivity(), psychologistList);
+        binding.rvPsychologist.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.rvPsychologist.setAdapter(adapter);
     }
 }
